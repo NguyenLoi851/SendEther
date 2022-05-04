a520169b4fefe112851da7414146d4da65d26874
NguyenLoi851 <nguyensyloi851@gmail.com>
2022-05-04 10:10:28 +0700

    add file .secret

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,11 +1,12 @@
 require('@nomiclabs/hardhat-waffle');
-
+const fs = require("fs");
+const priKey = fs.readFileSync('.secret').toString();
 module.exports = {
   solidity: '0.8.0',
   networks: {
     ropsten: {
       url: 'https://eth-ropsten.alchemyapi.io/v2/6xJ1bS6gSlpm3_FThTWQZO_DB84wfXRt',
-      accounts: ['825aeb4ef63ec8361d180dabbff5006166c749b08dcb8d31aced385522c4704b']
+      accounts: [priKey]
     }
   }
 }
